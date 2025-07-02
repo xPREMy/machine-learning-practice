a5c1924b17fe60e1fd9de7b294f3f72af785af09
xPREMy <b24bb1028@iitj.ac.in>
2025-07-02 22:38:48 +0530

    Update nnfasionmnist.py

--- fasion mnist/nnfasionmnist.py
+++ fasion mnist/nnfasionmnist.py
@@ -6,24 +6,25 @@ from sklearn.metrics import accuracy_score
 from keras.losses import SparseCategoricalCrossentropy
 from keras.layers import Dropout
 from keras.layers import BatchNormalization
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+from keras.regularizers import l2
 # data frame loading for training set
-df=pd.read_csv('fashion-mnist_test.csv')
+df=pd.read_csv(r'C:\Users\OKKKK\Desktop\stocktesting2\fasion mnist\fashion-mnist_train.csv')
 X_train=df.drop('label',axis=1).astype('float32').values/255.0
 Y_train=df['label'].astype('int32').values
 # data frame loading for test set
-df1=pd.read_csv('fashion-mnist_train.csv')
+df1=pd.read_csv(r'C:\Users\OKKKK\Desktop\stocktesting2\fasion mnist\fashion-mnist_test.csv')
 X_test=df1.drop('label',axis=1).astype('float32').values/255.0
 Y_test=df1['label'].astype('int32').values
 
 model=Sequential([
-    Dense(256,input_shape=(X_train.shape[1],),activation='sigmoid'),
-    BatchNormalization(),
-    Dropout(0.3),
-    Dense(64,'sigmoid'),
+    Dense(64,input_shape=(X_train.shape[1],),activation='relu',kernel_regularizer=l2(0.0001)),
+    Dense(32,'relu',kernel_regularizer=l2(0.0001)),
     Dense(10,'linear')
 ])
-model.compile(loss=SparseCategoricalCrossentropy(from_logits=True),optimizer='adam',metrics=['accuracy'])
-model.fit(X_train,Y_train,epochs=40,validation_split=0.1)
+model.compile(loss=SparseCategoricalCrossentropy(from_logits=True),optimizer=Adam(0.0001),metrics=['accuracy'])
+model.fit(X_train,Y_train,epochs=30,validation_split=0.1)
 
 logits=model.predict(X_test)
 prediction=np.argmax(logits,axis=1)
